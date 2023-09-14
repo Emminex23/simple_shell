2cbd60190784f94cccb44ca8ea95bc475af2dd63
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-09-14 15:41:32 -0700

    Add a Beautiful Code for Betty and Julien

---
+++ beautifulcode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a welcome message
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("Hello World! Welcome to the gates of Shell!\n");
+	return (0);
+}
