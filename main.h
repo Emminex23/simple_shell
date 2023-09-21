e024a38473cc68b4318b6a7650b85dcc69f4463c
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-09-21 00:40:32 -0700

    Update main.h & _print.c

--- main.h
+++ main.h
@@ -15,20 +15,20 @@
 #define BUFFERSIZE 1024
 
 /*Prototypes*/
-int exe(char *command);
-int findpath(const char *command, char *path, size_t length);
-unsigned int _strlen(const char *str);
-unsigned int _print(char *str);
-int _strncmp(const char *s1, const char *s2, size_t n);
-char *_strdup(const char *src);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *_strncpy(char* dest, const char* src, size_t n);
-void exit_shell(void);
-void print_env(void);
-int _strcmp(const char *str1, const char *str2);
-void handle_command(char *buffer);
-void handle_read_error(void);
-
+int exe(char *command); /*shell*/
+int findpath(const char *command, char *path, size_t length);/*find path*/
+unsigned int _strlen(const char *str); /*print*/
+unsigned int _print(char *str);/*print*/
+int _strncmp(const char *s1, const char *s2, size_t n);/*helper*/
+char *_strdup(const char *src); /*helper*/
+void *_memcpy(void *dest, const void *src, size_t n); /*helper*/
+char *_strncpy(char *dest, const char *src, size_t n);/*helper*/
+void exit_shell(void); /*helper*/
+void print_env(void); /*print*/
+int _strcmp(const char *str1, const char *str2); /*print*/
+void handle_command(char *buffer);/*helper*/
+void handle_read_error(void);/*helper*/
+int main(void);/*entrypoint*/
 extern char **environ;
 
 
