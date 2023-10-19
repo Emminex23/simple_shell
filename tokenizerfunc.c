8f2075c3c6f1f8bbc13ab30648af38ea7dc739a5
Emmanuel Oyibo <emminexworld@gmail.com>
2023-10-19 01:18:00 +0100

    Add some quick fix

--- tokenizerfunc.c
+++ tokenizerfunc.c
@@ -12,24 +12,40 @@
 char *tokenisation(char *str, char delimiter)
 {
 	static char *next_token;
-	char *token = next_token;
+	char *token_start;
 
 	if (str != NULL)
 	{
 		next_token = str;
 	}
+
 	if (next_token == NULL || *next_token == '\0')
 	{
 		return (NULL);
 	}
+
+	token_start = next_token;
+	while (*token_start == delimiter)
+	{
+		token_start++;
+	}
+
+	if (*token_start == '\0')
+	{
+		next_token = NULL;
+		return (NULL);
+	}
+
+	next_token = token_start;
 	while (*next_token != delimiter && *next_token != '\0')
 	{
 		next_token++;
 	}
+
 	if (*next_token == delimiter)
 	{
 		*next_token = '\0';
 		next_token++;
 	}
-	return (token);
+	return (token_start);
 }
